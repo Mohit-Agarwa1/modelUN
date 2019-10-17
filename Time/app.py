f721c85ac291a13b0c635d62c05c043b55636ee4
Mohit Agarwal <46870579+Mohit-Agarwa1@users.noreply.github.com>
2019-10-17 19:59:56 +0100

    Add files via upload

--- Time/app.py
+++ Time/app.py
@@ -1,169 +1,29 @@
+from flask import Flask, render_template, request
+import webbrowser
+import time
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-import base64
-import os
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import getpass
-from cryptography.fernet import Fernet
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.PublicKey import RSA
-from binascii import hexlify
+app = Flask(__name__)
 
-USERDATA_LOGIN = [['0000 0000 0000 0001','3'],['0000 0000 0000 0002','4']]
-listusers = []
-for i in USERDATA_LOGIN:
-    listusers.append(i[0])
 
-def send_mess(a,b):
-    print(a)
-def sendcodes(a):
-    print(a)
-def mohit_symmetric(a,b):
-    b[4] = b[4].split('\n')
-    if a == 'e':
-        print('Encryption Symmetric')
-        list_keys = []
-        for i in b[4]:
-            password_provided = i
-            password = password_provided.encode() # Convert to type bytes
-            salt = b'{enaluronarosegnotobiote0choir9cohuneff9ecttypalccolportage79enaluronempiriocritcismgarlandlesst9unmotivatedlyzjmercership0faithlesswaderscompilement64retroinfectionbugbite{W8{blasphemedpurposefulness9Chlmacanalmiqueretroinfection-missorting]2blasphemed4Dnontimberedpantheismstannotypeelimnichyacinthsm' # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
-            kdf = PBKDF2HMAC(
-                algorithm=hashes.SHA256(),
-                length=32,
-                salt=salt,
-                iterations=100000,
-                backend=default_backend()
-            )
-            key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
-            list_keys.append(key)
-        message = b[3].encode()
-        for i in list_keys:
-            f = Fernet(key)
-            message = f.encrypt(message)
-        print('hi')
-        print(message)
 
-    if a == 'd':
-        print('Decryption Symmetric')
-        list_keys = []
-        for i in b[4]:
-            password_provided = i
-            password = password_provided.encode() # Convert to type bytes
-            salt = b'{enaluronarosegnotobiote0choir9cohuneff9ecttypalccolportage79enaluronempiriocritcismgarlandlesst9unmotivatedlyzjmercership0faithlesswaderscompilement64retroinfectionbugbite{W8{blasphemedpurposefulness9Chlmacanalmiqueretroinfection-missorting]2blasphemed4Dnontimberedpantheismstannotypeelimnichyacinthsm' # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
-            kdf = PBKDF2HMAC(
-                algorithm=hashes.SHA256(),
-                length=32,
-                salt=salt,
-                iterations=100000,
-                backend=default_backend()
-            )
-            key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
-            list_keys.append(key)
-        message = b[3].encode()
-        for i in list_keys:
-            f = Fernet(key)
-            message = f.decrypt(message)
-        send_mess(message.decode(),b)
+@app.route("/")
+def home():
+    return render_template('RLEAPP.html')
 
-def mohit_asymmetric(a,b):
-    if a == 'e':
-        pu_key = RSA.import_key(b[4])
+@app.route("/display_input")
+def display_input():
+    return render_template('open_file.html')
 
-        inp = b[3]
-
-        message = inp.encode()
-        #Instantiating PKCS1_OAEP object with the public key for encryption
-        cipher = PKCS1_OAEP.new(key=pu_key)
-        #Encrypting the message with the PKCS1_OAEP object
-        cipher_text = cipher.encrypt(message)
-        b[3] = cipher_text
-        cipher_text
+@app.route('/display_input', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    text = open(text, 'r')
+    text = text.read()
+    text = text.replace('\n','<br>')
+    return text
 
 
 
 
-    if a == 'd':
-        print('Decryption Asymmetric')
-        pr_key = RSA.import_key(b[4])
-        cipher_text = b[3].encode()
-        print(cipher_text)
-        #Instantiating PKCS1_OAEP object with the private key for decryption
-        decrypt = PKCS1_OAEP.new(key=pr_key)
-        #Decrypting the message with the PKCS1_OAEP object
-        decrypted_message = decrypt.decrypt(cipher_text)
-        print(decrypted_message.decode())
-
-
-
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-
-app.layout = html.Div([
-    dcc.Input(id='input-1-state', type='text', value='PIC NUMBER'),
-    dcc.Input(id='input-2-state', type='text', value='PIC PIN'),
-    html.Button(id='submit-button', n_clicks=0, children='Submit'),
-    html.Div(id='output-state'),
-    dcc.Textarea(id='input-3-state', style={'width':'80%','height':'50%'},value='INPUT MESSAGE'),
-
-    dcc.RadioItems(id = 'input-5-state',options=[{'label': 'Symmetric', 'value': 'NYC'},{'label': 'Asymmetric', 'value': 'MTL'}],value='NYC'),
-    dcc.Textarea(id='input-4-state', style={'width':'80%','height':'50%'},value='DATA KEY(S), please seperate each key with a line in Symmetric, asymtric takes only one key, public to encode, private to decode. Enter the FULL KEY, with start and end clauses'),
-    dcc.RadioItems(id = 'input-6-state',options=[{'label': 'Encrypt', 'value': 'NYC'},{'label': 'Decrypt', 'value': 'MTL'}],value='NYC')
-
-])
-
-
-@app.callback(Output('output-state', 'children'),
-              [Input('submit-button', 'n_clicks')],
-              [State('input-1-state', 'value'),
-               State('input-2-state', 'value'),
-               State('input-3-state', 'value'),
-               State('input-4-state', 'value'),
-               State('input-5-state', 'value'),
-               State('input-6-state', 'value')])
-
-
-
-def update_output(n_clicks, input1, input2,input3,input4,input5,input6):
-    print([n_clicks, input1, input2,input3,input4,input5,input6])
-    listENCRYPTDETAILS = [n_clicks,input1, input2,input3,input4,input5,input6]
-    if n_clicks == 0:
-        return "Please login with your PIC ID and PIC PIN NUMBER\nPlease note: your PIC ID must include spaces"
-    if n_clicks > 2:
-        print('OauthERROR')
-    if input1 in listusers:
-        print('USERID')
-        if [input1,input2] in USERDATA_LOGIN:
-            if input3 == 'INPUT MESSAGE' or input3=='SEND CODES':
-                return('Successful login,\nYou may now use the encryption/decryption tool, all results will be emailed to you.')
-                if input3 == 'SEND CODES':
-                    sendcodes(listENCRYPTDETAILS)
-            else:
-
-
-
-                if input5 == "NYC":
-
-                    if input6 == "NYC":
-                        mohit_symmetric('e',listENCRYPTDETAILS)
-                    if input6 == "MTL":
-                        mohit_symmetric('d',listENCRYPTDETAILS)
-                if input5 == "MTL":
-                    if input6 == "NYC":
-                        mohit_asymmetric('e',listENCRYPTDETAILS)
-                    if input6 == "MTL":
-                        mohit_asymmetric('d',listENCRYPTDETAILS)
-                return('Order Executed,Result will be emailed to your PIC accout')
-        else:
-            return 'Login failed'
-    else:
-        return 'Login failed'
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Time/static/sketchSphere.js
@@ -0,0 +1,92 @@
+let sketch = function(p) {
+  let rows = Math.trunc( (window.innerHeight*window.innerWidth)/72000 );;
+  let radius = Math.trunc( (window.innerHeight*window.innerWidth)/6000);
+  let min_length = 35;
+  let max_length = 85;
+  let space = 15;
+  let stripes = [];
+  let colors;
+  console.log(rows,radius,min_length,max_length,space)
+  p.setup = function() {
+
+    let WIN_HEIHGT_SET_MOHIT = window.innerHeight *0.85
+    let WIN_WIDTHH_SET_MOHIT = window.innerWidth *0.85
+    p.createCanvas(WIN_WIDTHH_SET_MOHIT,WIN_HEIHGT_SET_MOHIT);
+    p.stroke(255);
+    p.strokeWeight(14);
+    //p.noLoop();
+
+    colors = [
+      p.color(142,192,124),
+      p.color(250,189,47),
+      p.color(251,71,44),
+      p.color(211,134,147),
+      p.color(49,69,80),
+      p.color(054, 057, 059),
+      p.color(202, 207, 214),
+      p.color(081, 118, 100),
+      p.color(053, 079, 082),
+      p.color(219, 138, 135)
+    ];
+
+    for (var i = 0; i < rows; i++) {
+      let ypos = ((i + .5)/rows) * (radius * 2) - radius;
+      let row_length = get_row_length(ypos);
+      add_stripe_row(ypos, row_length);
+    }
+  }
+
+  p.draw = function() {
+    p.clear();
+    p.translate(p.width / 2, p.height / 2);
+    for (var row in stripes) {
+      for(var s in stripes[row]){
+        var stripe = stripes[row][s];
+        let length = get_row_length(stripe.y);
+        if(!is_outside_circle(stripe, length)){
+          p.stroke(stripe.color);
+          p.line(p.max((stripe.start+space), -length), stripe.y, p.min((stripe.end-space), length), stripe.y);
+        } else if (stripe.start > length){
+            stripes[row].splice(s,1);
+            let s_length = p.random(min_length,max_length);
+            let end = stripes[row][0].start;
+            let start = end - s_length;
+            stripes[row].unshift({y:stripe.y, start:start, end:end, color:colors[p.floor(p.random(10))]});
+        }
+          let startx = p.constrain(stripe.start, -length, length);
+          let endx = p.constrain(stripe.end, -length, length);
+          let startspeed = p.sqrt(2) - p.sqrt(stripe.y * stripe.y + startx * startx) / radius;
+          let endspeed = p.sqrt(2) - p.sqrt(stripe.y * stripe.y + endx * endx) / radius;
+          stripe.start += startspeed;
+          stripe.end += endspeed;
+
+      }
+    }
+  }
+
+  function get_row_length(ypos) {
+    if ((radius * radius) < (ypos * ypos)) return 0;
+    return p.sqrt((radius * radius) - (ypos * ypos));
+  }
+
+  function is_outside_circle(stripe, length) {
+    return stripe.end-space < -length || stripe.start+space > length;
+  }
+
+  function add_stripe_row (ypos, row_length) {
+    let row = [];
+    let length = p.random(min_length,max_length);
+    let start = -1000 + p.random(min_length,max_length);
+    let end = start + length;
+    while (end < -row_length) {
+      row.push({y:ypos, start:start, end:end, color:colors[p.floor(p.random(10))]});
+      length = p.random(min_length,max_length);
+      start = end;
+      end = start + length;
+    }
+    stripes.push(row);
+  }
+
+}
+
+new p5(sketch);
